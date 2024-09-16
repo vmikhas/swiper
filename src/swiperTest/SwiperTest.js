e19e88dd31f50b5493e0b7b6f34621b42ca498df
vmikhas <vmikhas@yandex.ru>
2024-09-16 18:24:43 +0300

    правка

--- src/swiperTest/SwiperTest.js
+++ src/swiperTest/SwiperTest.js
@@ -15,17 +15,18 @@ export default function SwiperTest({images, arrow}) {
     <div className={"swiper"}>
       <h1 className={"swiper__title"}>Swiper</h1>
 
-      <Swiper className={"swiper__container"}
-              slidesPerView={1}
-              loop={true}
-              pagination={{clickable: true}}
-              onSwiper={(swiper) => {
-                swiper.params.navigation.prevEl = prevRef.current;
-                swiper.params.navigation.nextEl = nextRef.current;
-                swiper.navigation.init();
-                swiper.navigation.update();
-              }}
-              modules={[Pagination, Navigation]}>
+      <Swiper
+        className={"swiper__container"}
+        slidesPerView={1}
+        loop={true}
+        pagination={{clickable: true}}
+        onSwiper={(swiper) => {
+          swiper.params.navigation.prevEl = prevRef.current;
+          swiper.params.navigation.nextEl = nextRef.current;
+          swiper.navigation.init();
+          swiper.navigation.update();
+        }}
+        modules={[Pagination, Navigation]}>
         {images && images.map((item, id) => (
           <SwiperSlide className={"swiper__slide"} key={id + 1}>
             <div className={`swiper__image swiper__image_${id + 1}`} key={id + 1}>
